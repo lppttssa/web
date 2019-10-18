7b25fca9cb1a4640b87676db3427bcad85f5c35e
lppttssa <lppttssa@gmail.com>
2019-10-18 20:31:09 +1000

    first commit

---
+++ 1.4ArrSort/1.4ArrSort/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace _1._4ArrSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int size = 20;
+            int[] arr = new int[size];
+            for (int i = 0; i <size; i++)
+            {
+                arr[i] = random.Next(1, 31);
+                Console.Write(arr[i] + " ");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Как Вы хотите отсортировать массив? " +
+                "Введите max - от максимального, min - от минимального: ");
+            string how = Console.ReadLine();
+
+            int temp;
+            if (how == "min")
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = size - 1; j > i; j--)
+                    {
+                        if (arr[j - 1] > arr[j])
+                        {
+                            temp = arr[j - 1];
+                            arr[j - 1] = arr[j];
+                            arr[j] = temp;
+                        }
+                    }
+                }
+            } 
+            else
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = size - 1; j > i; j--)
+                    {
+                        if (arr[j - 1] < arr[j])
+                        {
+                            temp = arr[j - 1];
+                            arr[j - 1] = arr[j];
+                            arr[j] = temp;
+                        }
+                    }
+                }
+            }
+           
+            for (int j = 0; j < size; j++)
+            {
+                Console.Write(arr[j] + " ");
+            }
+        }
+    }
+}
